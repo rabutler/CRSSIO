5669b24cd5ad0dd7aa887a4a490b3cb85579cb98
Alan Butler <rabutler@usbr.gov>
2018-03-20 12:31:29 -0600

    initial conversion from Ken's script into a function.
    - updated some variable names
    - changed = to <-
    - and optionally save the data
    - return list of disagg data and index years

--- R/paleo_disag.R
+++ R/paleo_disag.R
@@ -1,183 +1,198 @@
 
-#this is a direct resampling of LB tribs - no scale factor applied
-rm(list=ls())
+#' Spatial and temporal diaggregattion of paleo flow data
+#' 
+#' This is a direct resampling of LB tribs, i.e., no scale factor is applied to
+#' them.
+#' 
+#' @param x The annual paleo data to disaagregate.
+#' @param ann_flw Observed annual flow data used for picking analog year.
+#' @param mon_flw Intervening monthly natural flow. Used for spatially and 
+#'   temporaly disaggregating paleo data based on the index year.
+#' @param nsite The number of sites to diaggregate the data too. 
+#' @param nsim Number of times to repeat the space/time disaggregation.
+#' 
+#' @examples 
+#' \dontrun{
+#' # read in annual synthetic mon_flw for disag
+#' x <- matrix(scan("data-raw/Meko.txt"), ncol = 2, byrow = TRUE) 
+#' # intervening natural flow mon_flw - monthly CY text file
+#' mon_flw <- matrix(
+#'   scan("data-raw/CRB_CY_MON_IV_29_06_08.txt"), 
+#'   ncol = 29, 
+#'   byrow = TRUE
+#' )
+#' 
+#' # observed annual flow for picking analog disag yr
+#' ann_flw <- matrix(scan("data-raw/LF_06_08.txt"), ncol = 2, byrow = TRUE)
+#' zz <- paleo_disagg(x, ann_flw, mon_flw, 29, 1)
+#' }
+#' 
+#' @export
+paleo_disagg <- function(x, ann_flw, mon_flw, nsite, nsim, ofolder = NULL)
+{
+  n_paleo_yrs <- nrow(x) # 1244 for meko; length of each simulation (yrs)
+  
+  # how many yrs of observed mon_flw
+  n_obs_yrs <- nrow(mon_flw)/12 
+  
+  if (n_obs_yrs != nrow(ann_flw))
+    stop("`ann_flw` and `mon_pttrn` must have the same number of years.")
+  
+  if (nsite != ncol(mon_flw))
+    stop("`mon_flow` needs to have `nsite` columns.")
+  
+  # matrix for observed values - row(yr), col (month), index (site)
+  dat_a <- array(data = NA, dim=c(n_obs_yrs, 12, nsite)) 
 
-nsim=1 # number of simulations	
+  # matrix for disag values - row(yr), col (month), index1 (site), index2(sim #)
+  disag <- array(data = NA, dim = c(n_paleo_yrs, 12, nsite, nsim))
+  
+  # matrix for recording yr index for disag (optional)
+  index_mat <- matrix(ncol = nsim, nrow = n_paleo_yrs) 
+  
+  # matrix for recording scale factors used (optional)
+  sf_mat <- matrix(ncol = nsim, nrow = n_paleo_yrs)
+  
+  # this loop moves observed monthly mon_flw from 2d matrix to 3d array
+  mgn <- length(dat_a[1,1,])
+  
+  for(j in 1:mgn){
+  
+    s <- 1
+    e <- 12
 
+  	for(i in 1:n_obs_yrs){
+  
+  	  dat_a[i,,j] <- mon_flw[s:e,j]
+  
+  	  s <- s + 12
+  	  e <- e + 12
+  	}
+  }
+  
+  # temporary matrix for storing time only disag
+  temp <- array(data = NA, dim = c(n_paleo_yrs, 12, nsim)) 
+  
+  for(j in 1:nsim){
+  
+    #this picks the 1st year for disag based only on the annual flow
+  	k <- sqrt(n_obs_yrs) #number of neighbors
+  	
+  	Flow <- x[1,(j+1)]
+  	
+  	D <- abs(ann_flw[,2] - Flow)
+  	
+  	# combines difference and corresponding year into one matrix
+  	Delta <- cbind(ann_flw[,1], D) 
+  	
+  	# reorders the delta matrix based on distances
+  	Delta_sort <- cbind(Delta[,1][order(Delta[,2])], sort(Delta[,2])) 
+  	
+  	# selects the "k-nearest-neighbors" from Delta_sort 
+  	kmatrix <- Delta_sort[1:k,1:2] 
+  	
+  	# defines matrix for weights
+  	weight <- matrix(nrow=k, ncol=1) 
+   	
+  	# ranks distances for purpose of generating weights	
+  	rnk <- rank(kmatrix[,2]) 
+  		
+  		for(i in 1:k){
+  		  # fills weighting matrix
+  			weight[i,1] <- 1/(rnk[i]) 
+  	
+  		}
+  
+  	z <- sum(weight) # sums weights 
+  
+  	# divides weights by sum of weights so cumulative probability = 1
+  	weights <- weight/z	
+  	
+  	# Selects a year to be "nearest neighbor"
+  	N <- sample(kmatrix[,1], 1, replace = TRUE, prob=weights) 
+  
+  	# index for selected yr
+  	pos <- N - (ann_flw[1,1] - 1) 
+    
+  	# scaling factor to apply for disag
+  	SF <- Flow/(ann_flw[pos,2]) 
+  	temp[1, ,j] <- dat_a[pos, ,mgn] * SF	
+  	index_mat[1,j] <- N
+  	sf_mat[1,j] <- SF
+    disag[1, ,1:20,j] <- dat_a[pos, ,1:20]*SF
+  	disag[1, ,21:29,j] <- dat_a[pos, ,21:29]
+  
+    # now that one year has been disaggregated, the remaining years in the 
+  	# trace use annual flow and also december of last yr (CY)
+  	for(h in 2:n_paleo_yrs){
+  
+  		k <- sqrt(n_obs_yrs) #number of neighbors
+  
+  		Flow <- x[h,(j + 1)]
+  		D <- 2:n_obs_yrs
+  	
+  		# annual as the only selection criteria
+      D <- abs(ann_flw[,2] - Flow)
+      
+      # combines difference and corresponding year into one matrix 
+      # these use just m.a.f
+  		Delta <- cbind(ann_flw[,1],D) 
+  
+  		# reorders the delta matrix based on distances
+  		Delta_sort <- cbind(Delta[,1][order(Delta[,2])], sort(Delta[,2])) 
+  		
+  		# selects the "k-nearest-neighbors" from Delta_sort
+  		kmatrix <- Delta_sort[1:k,1:2]  
+  	  weight <- matrix(nrow=k, ncol=1) # defines matrix for weights
+   		
+  	  # ranks distances for purpose of generating weights
+  		rnk <- rank(kmatrix[,2]) 
+  		
+  		for(i in 1:k){
+  	
+  			weight[i,1] <- 1/(rnk[i]) #fills weighting matrix
+  	
+  		}
+  
+  		z <- sum(weight) # sums weights 
+  	
+  		#divides weights by sum of weights so cumulative probability = 1
+  		weights <- weight/z	
+  		
+  		# Selects a year to be "nearest neighbor"
+  		N <- sample(kmatrix[,1], 1, replace = TRUE, prob = weights) 
+  		pos <- N - (ann_flw[1,1] - 1) # index for selected yr
+  		SF <- Flow/(ann_flw[pos,2]) # scaling factor to apply for disag
+  
+  		#disag[h, , ,j]=dat_a[pos, ,]*SF
+      disag[h, ,1:20,j]=dat_a[pos, ,1:20]*SF
+  		disag[h, ,21:29,j]=dat_a[pos, ,21:29]
+  	}
+  }
+  			
+  # output to "flat" file
+  
+  disagmat=matrix(ncol=nsite, nrow=(n_paleo_yrs*12*nsim))
+  
+  for(i in 1:nsite){
+    p <- 1
+  
+    for(k in 1:nsim){
+      for(j in 1:n_paleo_yrs){
+        disagmat[p:(p+11),i] <- disag[j,,i,k]
+        p <- p + 12
+      }
+    }
+  }
+  
+  if (!is.null(ofolder)) {
+    write.csv(
+      t(disagmat), 
+      file = file.path(ofolder, "paleo_disagg.csv"), 
+      ncol = nsite
+    )
+    write.csv(index_mat, file = file.path(ofolder, "index_years.csv"))
+  }
 
-l=1244# length of each simulation (yrs)
-
-nsite=29
-
-#dataD= matrix(scan("input/LF_total.txt"), ncol=20, byrow=TRUE) # obs data to be used for disag (total flow) 20 cols, monthly data #(20x1212)
-
-data  =matrix(scan("input/CRB_CY_MON_IV_29_06_08.txt"), ncol=29, byrow=T) #intervening natural flow data - monthly CY text file
-
-
-an_flw= matrix(scan("input/LF_06_08.txt"), ncol=2, byrow=TRUE) # observed annual flow for picking analog disag yr
-
-al=length(an_flw)
-
-
-len=length(data[,1])/12 # how many yrs of observed data
-
-
-dat_a= array(data=NA,dim=c(len,12,nsite)) # matrix for observed values - row(yr), col (month), index (site)
-
-pp=4
-
-
-
-disag=array(data=NA,dim=c(l,12,nsite,nsim)) #matrix for disag values - row(yr), col (month), index1 (site), index2(sim #)
-
-index_mat=matrix(ncol=nsim, nrow=l) # matrix for recording yr index for disag (optional)
-
-sf_mat= matrix(ncol=nsim, nrow=l)#matrix for recording scale factors used (optional)
-
-#this loop moves observed monthly data from 2d matrix to 3d array
-
-mgn=length(dat_a[1,1,])
-
-for(j in 1:mgn){
-
-s=1
-e=12
-
-	for(i in 1:len){
-
-	dat_a[i,,j]=data[s:e,j]
-
-	s=s+12
-	e=e+12
-	}
-
+  invisible(list(paleo_disagg = t(disagmat), index_yrs = index_mat))
 }
-
-seqs= matrix(scan("input/Meko.txt"), ncol=2, byrow=T) #read in annual synthetic data for disag
-
-
-temp=array(data=NA,dim=c(l,12,nsim)) # temporary matrix for storing time only disag
-
-
-for(j in 1:nsim){
-
-#this picks the 1st year for disag based only on the annual flow
-	k=sqrt(len) #number of neighbors
-	
-	Flow=seqs[1,(j+1)]
-	
-	D=abs(an_flw[,2]-Flow)
-	
-	Delta=cbind(an_flw[,1],D) #combines difference and corresponding year into one matrix
-	
-	Delta_sort=cbind(Delta[,1][order(Delta[,2])], sort(Delta[,2])) #reorders the delta matrix based on distances
-	
-	kmatrix = Delta_sort[1:k,1:2] #selects the "k-nearest-neighbors" from Delta_sort 
-	
-	weight = matrix(nrow=k, ncol=1) # defines matrix for weights
- 		
-	rnk=rank(kmatrix[,2]) #ranks distances for purpose of generating weights
-		
-		for(i in 1:k){
-	
-			weight[i,1] = 1/(rnk[i]) #fills weighting matrix
-	
-		}
-
-	z = sum(weight) # sums weights 
-
-	weights = weight/z	#divides weights by sum of weights so cumulative probability = 1
-		
-	N=sample(kmatrix[,1], 1, replace = TRUE, prob=weights) #Selects a year to be "nearest neighbor"
-		
-
-	pos=N-(an_flw[1,1]-1) # index for selected yr
-
-	SF=Flow/(an_flw[pos,2]) # scaling factor to apply for disag
-
-	temp[1, ,j]=dat_a[pos,,mgn]*SF	
-
-	index_mat[1,j]=N
-	sf_mat[1,j]=SF
-
-	
-        disag[1, ,1:20,j]=dat_a[pos, ,1:20]*SF
-		disag[1, ,21:29,j]=dat_a[pos, ,21:29]
-
-# now that one year has been disaggregated, the remaining years in the trace use annual flow and also december of last yr (CY)
-
-		for(h in 2:l){
-
-
-		k=sqrt(len) #number of neighbors
-
-		Flow=seqs[h,(j+1)]
-		D=2:len
-	
-        		
-			D=abs(an_flw[,2]-Flow) # annual as the only selection criteria
-			Delta=cbind(an_flw[,1],D) #combines difference and corresponding year into one matrix #these use just m.a.f
-
-
-			Delta_sort=cbind(Delta[,1][order(Delta[,2])], sort(Delta[,2])) #reorders the delta matrix based on distances
-		
-			kmatrix = Delta_sort[1:k,1:2] #selects the "k-nearest-neighbors" from Delta_sort 
-	
-			weight = matrix(nrow=k, ncol=1) # defines matrix for weights
- 		
-			rnk=rank(kmatrix[,2]) #ranks distances for purpose of generating weights
-		
-			for(i in 1:k){
-	
-			weight[i,1] = 1/(rnk[i]) #fills weighting matrix
-	
-			}
-
-			z = sum(weight) # sums weights 
-	
-			weights = weight/z	#divides weights by sum of weights so cumulative probability = 1
-		
-			N=sample(kmatrix[,1], 1, replace = TRUE, prob=weights) #Selects a year to be "nearest neighbor"
-		
-			pos=N-(an_flw[1,1]-1) # index for selected yr
-	
-			SF=Flow/(an_flw[pos,2]) # scaling factor to apply for disag
-
-			
-       
-
-			#disag[h, , ,j]=dat_a[pos, ,]*SF
-       		disag[h, ,1:20,j]=dat_a[pos, ,1:20]*SF
-		    disag[h, ,21:29,j]=dat_a[pos, ,21:29]
-			}
-			}
-			
-
-
-## output to "flat" file
-
-
-disagmat=matrix(ncol=nsite, nrow=(l*12*nsim))
-
-for(i in 1:nsite){
-p=1
-
-for(k in 1:nsim){
-
-
-for(j in 1:l){
-disagmat[p:(p+11),i]=disag[j,,i,k]
-p=p+12
-}
-}
-}
-
-write(t(disagmat), file="output/paleo_disag_4crss.txt", ncol=nsite)
-
-#seqs=0
-
-#source("code\\plottingCode\\plot_all.rm")
-
-
-
-
